b18f355bf969d7de0cd1f4c3f779ec6c40389dab
ChanLee_KR <cl700120@gmail.com>
2024-12-01 19:56:49 +0900

    refactor: indexed db config 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
+import localForage from 'localforage'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/app'
 import { getMainElement } from '@/shared/utils'
+
+localForage.config({
+  driver: localForage.INDEXEDDB,
+  name: 'PBL-Notes',
+  version: 1.0,
+  storeName: 'data',
+  description: 'my notes database',
+})
 
 const mainEl = getMainElement()
 const root = createRoot(mainEl)
