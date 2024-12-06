5ea4ba02dca19af7e4cfb3f961a54ac97470424e
ChanLee_KR <cl700120@gmail.com>
2024-12-06 14:42:12 +0900

    refactor: 활성 폴더 및 노트 로직 메모이제이션 적용

--- src/entities/folder/model/useActiveFolder.ts
+++ src/entities/folder/model/useActiveFolder.ts
@@ -1,13 +1,21 @@
+import React from 'react'
 import { useParams } from 'react-router'
 import type { FolderPath } from '@/shared/types'
-import { useFolderDB } from './folderStore'
+import { useFolderDB, useFolderSession } from './folderStore'
 
 export default function useActiveFolder() {
   const { folderId } = useParams()
-  const folderDB = useFolderDB()
   const targetId = +(folderId || -1)
-  const folder = folderDB.get(targetId) ?? null
-  const path: FolderPath | null = folder ? `/folder/${folder.id}` : null
 
-  return { folder, path }
+  const folderDB = useFolderDB()
+  const folder = React.useMemo(() => folderDB.get(targetId) ?? null, [folderDB, targetId])
+
+  const path = folder ? (`/folder/${folder.id}` satisfies FolderPath) : null
+
+  const folderSession = useFolderSession()
+  const isNotFound = React.useMemo(() => {
+    return folderSession && !folderDB.has(targetId)
+  }, [folderSession, folderDB, targetId])
+
+  return { folder, path, isNotFound }
 }
--- src/entities/note/model/useActiveNote.ts
+++ src/entities/note/model/useActiveNote.ts
@@ -1,12 +1,18 @@
+import React from 'react'
 import { useParams } from 'react-router'
-import { useNoteDB } from './noteStore'
+import { useNoteDB, useNoteSession } from './noteStore'
 
 export default function useActiveNote() {
   const { noteId } = useParams()
   const targetId = +(noteId || -1)
 
   const noteDB = useNoteDB()
-  const activeNote = noteDB.get(targetId) ?? null
+  const note = React.useMemo(() => noteDB.get(targetId) ?? null, [noteDB, targetId])
 
-  return { note: activeNote, noteId: targetId }
+  const noteSession = useNoteSession()
+  const isNotFound = React.useMemo(() => {
+    return noteSession && !noteDB.has(targetId)
+  }, [noteSession, noteDB, targetId])
+
+  return { note, noteId: targetId, isNotFound }
 }
