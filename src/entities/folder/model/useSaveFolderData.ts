7f72f8234584d8e6de28cb98664fe2c97eae26e8
ChanLee_KR <cl700120@gmail.com>
2024-12-09 12:52:06 +0900

    fix: 데이터 초기화 이슈 수정

--- src/entities/folder/model/useSaveFolderData.ts
+++ src/entities/folder/model/useSaveFolderData.ts
@@ -1,6 +1,7 @@
 import React from 'react'
 import { reportOnError } from '@/shared/utils'
 import { saveData } from '../utils'
+import { useFolderSession } from './folderStore'
 import useFolderList from './useFolderList'
 
 interface UseSaveFolderDataParams {
@@ -10,14 +11,17 @@ interface UseSaveFolderDataParams {
 
 export default function useAutoSaveFolderData({ onStart, onEnd }: UseSaveFolderDataParams) {
   const folderList = useFolderList()
+  const folderSession = useFolderSession()
 
   React.useEffect(() => {
-    onStart()
-    saveData(folderList)
-      .catch((error) => {
-        console.warn(error)
-        reportOnError('폴더 정보 저장에 실패했어요')
-      })
-      .finally(onEnd)
-  }, [folderList, onStart, onEnd])
+    if (folderSession) {
+      onStart()
+      saveData(folderList)
+        .catch((error) => {
+          console.warn(error)
+          reportOnError('폴더 정보 저장에 실패했어요')
+        })
+        .finally(onEnd)
+    }
+  }, [folderSession, folderList, onStart, onEnd])
 }
--- src/entities/note/model/useAutoSaveNoteData.ts
+++ src/entities/note/model/useAutoSaveNoteData.ts
@@ -2,6 +2,7 @@ import React from 'react'
 import { reportOnError } from '@/shared/utils'
 import { useDocumentEvent, useWindowEvent } from '@/shared/hooks'
 import { saveData } from '../utils'
+import { useNoteSession } from './noteStore'
 import useNoteList from './useNoteList'
 import useSettingTempNote from './useSettingTempNote'
 
@@ -12,19 +13,22 @@ interface UseSaveNoteDataParams {
 
 export default function useAutoSaveNoteData({ onStart, onEnd }: UseSaveNoteDataParams) {
   const noteList = useNoteList()
+  const noteSession = useNoteSession()
   const { note } = useSettingTempNote()
 
   const save = React.useCallback(() => {
-    onStart()
-    saveData(noteList)
-      .catch((error) => {
-        console.warn(error)
-        reportOnError('노트 정보 저장에 실패했어요')
-      })
-      .finally(onEnd)
-  }, [noteList, onStart, onEnd])
+    if (noteSession) {
+      onStart()
+      saveData(noteList)
+        .catch((error) => {
+          console.warn(error)
+          reportOnError('노트 정보 저장에 실패했어요')
+        })
+        .finally(onEnd)
+    }
+  }, [noteSession, noteList, onStart, onEnd])
 
-  React.useEffect(save, [noteList, save])
+  React.useEffect(save, [noteSession, noteList, save])
 
   useWindowEvent('beforeunload', (e) => {
     if (!note || !note.modifiable || note.isLocked) return
