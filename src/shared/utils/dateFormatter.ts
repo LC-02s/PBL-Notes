c66cdde3ff255ab6057a31f36e67978e50f0f5b7
ChanLee_KR <cl700120@gmail.com>
2024-12-12 14:24:39 +0900

    fix: 날짜 포맷 기능 동작 수정

--- src/shared/utils/dateFormatter.ts
+++ src/shared/utils/dateFormatter.ts
@@ -1,12 +1,10 @@
-import dayjs, { type ConfigType } from 'dayjs'
+import dayjs from 'dayjs'
 import 'dayjs/locale/ko'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(relativeTime)
 dayjs.locale('ko')
 
-export default function formatDateFromNow(date: ConfigType) {
-  const now = dayjs().format('YYYY-MM-DD')
-
-  return dayjs(date).from(now)
+export default function formatDateFromNow(date: number) {
+  return dayjs(date).fromNow()
 }
