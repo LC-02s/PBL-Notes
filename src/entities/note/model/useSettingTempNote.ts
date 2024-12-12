1e1c51695d6153e5d2b83c4bac218f9cce970e20
ChanLee_KR <cl700120@gmail.com>
2024-12-12 12:46:39 +0900

    fix: 링크로 페이지 접근 시 노트 출력 안되는 현상 수정

--- src/entities/note/model/useSettingTempNote.ts
+++ src/entities/note/model/useSettingTempNote.ts
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useSetTempNote } from './noteStore'
+import { useNoteSession, useSetTempNote } from './noteStore'
 import useActiveNote from './useActiveNote'
 
 export default function useSettingTempNote() {
   const { note, noteId } = useActiveNote()
+  const noteSession = useNoteSession()
   const setTempNote = useSetTempNote()
 
-  React.useEffect(() => setTempNote({ createAt: noteId }), [setTempNote, noteId])
+  React.useEffect(() => setTempNote({ createAt: noteId }), [setTempNote, noteId, noteSession])
 
   return { note }
 }
