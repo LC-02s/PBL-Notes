72bee6609ba6a325698243b7aa9d539f9109a406
ChanLee_KR <cl700120@gmail.com>
2024-12-12 14:47:41 +0900

    refactor: 노트 생성 기능 재 구현

---
+++ src/features/create-note/ui/CreateNoteButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+import { useActiveFolder } from '@/entities/folder'
+import { useActiveNote, useAddNote, useTempNote } from '@/entities/note'
+import type { NotePath, NotePathInFolder } from '@/shared/types'
+import { HiddenText, Icon, MenuButton } from '@/shared/ui'
+import { getNow } from '@/shared/utils'
+
+export default function CreateNoteButton() {
+  const navigate = useNavigate()
+
+  const { folder, path } = useActiveFolder()
+  const addNote = useAddNote()
+
+  const add = React.useCallback(() => {
+    const createAt = getNow()
+    const targetPath: NotePathInFolder | NotePath = `${path ?? ''}/note/${createAt}`
+
+    addNote({ createAt, included: folder?.id, onSuccess: () => navigate(targetPath) })
+  }, [addNote, navigate, folder, path])
+
+  const { note } = useActiveNote()
+  const tempNote = useTempNote()
+  const isDisabled = !!note && !!tempNote && (!tempNote.title || !tempNote.markdown)
+
+  return (
+    <MenuButton title="노트 생성" disabled={isDisabled} onClick={add}>
+      <Icon.PenNewSquareOutline />
+      <HiddenText>노트 생성</HiddenText>
+    </MenuButton>
+  )
+}
