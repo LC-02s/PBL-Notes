07def3baba4a0e29b7a588ff01de45c2caa2cc5f
ChanLee_KR <cl700120@gmail.com>
2024-12-17 00:02:55 +0900

    design: 위젯 연결 및 퍼블리싱

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,22 @@
+import { FolderSection } from '@/widgets/folder-section'
+import { Menu } from '@/widgets/menu'
+import { NoteSection } from '@/widgets/note-section'
 import Provider from './Provider'
 
 import './App.css'
 
 export default function App() {
-  return <Provider />
+  return (
+    <Provider>
+      <FolderSection />
+      <section className="flex size-full flex-1 flex-col items-stretch justify-between border-l border-gray200 bg-gray000 transition-colors">
+        <div className="block h-12 w-full">
+          <Menu />
+        </div>
+        <article className="block size-full max-h-[calc(100%-3rem)] flex-1">
+          <NoteSection />
+        </article>
+      </section>
+    </Provider>
+  )
 }
