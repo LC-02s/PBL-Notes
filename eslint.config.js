82989f5cb34834b3774c9abda34205ae009c6199
ChanLee_KR <cl700120@gmail.com>
2024-12-17 12:14:38 +0900

    style: eslint config 표기 통합

--- eslint.config.js
+++ eslint.config.js
@@ -12,101 +12,99 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 import tailwindcss from 'eslint-plugin-tailwindcss'
 
 export default [
-  ...typescriptEslint.config(
-    { ignores: ['dist'] },
-    {
-      extends: [js.configs.recommended, ...typescriptEslint.configs.recommended],
-      files: ['**/*.{ts,tsx}'],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-        parser: typescriptParser,
-        parserOptions: {
-          sourceType: 'module',
-          project: ['./tsconfig.node.json', './tsconfig.app.json'],
-          tsconfigRootDir: import.meta.dirname,
-        },
-      },
-      plugins: {
-        react,
-        'react-hooks': reactHooks,
-        'react-refresh': reactRefresh,
-        'jsx-a11y': jsxA11y,
-        import: importPlugin,
-        prettier,
-      },
-      rules: {
-        'import/order': [
-          'warn',
-          {
-            groups: [
-              'builtin',
-              'external',
-              'internal',
-              'type',
-              'parent',
-              'sibling',
-              'index',
-              'unknown',
-            ],
-            pathGroups: [
-              {
-                pattern: 'react*',
-                group: 'external',
-                position: 'before',
-              },
-              {
-                pattern: '@/app*',
-                group: 'internal',
-                position: 'after',
-              },
-              {
-                pattern: '@/widgets/*',
-                group: 'internal',
-                position: 'after',
-              },
-              {
-                pattern: '@/features/*',
-                group: 'internal',
-                position: 'after',
-              },
-              {
-                pattern: '@/entities/*',
-                group: 'internal',
-                position: 'after',
-              },
-              {
-                pattern: '@/shared/*',
-                group: 'internal',
-                position: 'after',
-              },
-            ],
-            pathGroupsExcludedImportTypes: ['react-router', 'react-hook-form', 'react-toastify'],
-            alphabetize: { order: 'asc' },
-          },
-        ],
-        'import/no-unresolved': 'off',
-        '@typescript-eslint/consistent-type-exports': 'error',
-        '@typescript-eslint/consistent-type-imports': 'error',
-        ...react.configs.recommended.rules,
-        ...react.configs['jsx-runtime'].rules,
-        'react/jsx-props-no-spreading': 'off',
-        'react/require-default-props': 'off',
-        'react/prop-types': 'off',
-        'react/display-name': 'off',
-        'react/no-unknown-property': ['error', { ignore: ['css'] }],
-        'react/self-closing-comp': 'warn',
-        ...reactHooks.configs.recommended.rules,
-        ...jsxA11y.configs.recommended.rules,
-        'jsx-a11y/label-has-associated-control': ['error', { some: ['nesting', 'id'] }],
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        'prettier/prettier': 'warn',
-      },
-      settings: {
-        react: { version: 'detect' },
+  ...typescriptEslint.config({
+    ignores: ['dist'],
+    extends: [js.configs.recommended, ...typescriptEslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parser: typescriptParser,
+      parserOptions: {
+        sourceType: 'module',
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-  ),
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      import: importPlugin,
+      prettier,
+    },
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'type',
+            'parent',
+            'sibling',
+            'index',
+            'unknown',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react*',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/app*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/widgets/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/features/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/entities/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/shared/*',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react-router', 'react-hook-form', 'react-toastify'],
+          alphabetize: { order: 'asc' },
+        },
+      ],
+      'import/no-unresolved': 'off',
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'react/jsx-props-no-spreading': 'off',
+      'react/require-default-props': 'off',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      'react/no-unknown-property': ['error', { ignore: ['css'] }],
+      'react/self-closing-comp': 'warn',
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'jsx-a11y/label-has-associated-control': ['error', { some: ['nesting', 'id'] }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  }),
   ...tailwindcss.configs['flat/recommended'],
   eslintConfigPrettier,
 ]
